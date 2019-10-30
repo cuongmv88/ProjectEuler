f834fe7d01b58f8efd8ae3ef15aaf38e99314039
Cuong Mai Van <cuongmv@inmeeting.com.vn>
2019-10-31 00:22:16 +0700

    format code

--- problem38.py
+++ problem38.py
@@ -1,13 +1,15 @@
 import time
 
-def calculate_time(func): 
-    def inner1(*args, **kwargs): 
-        begin = time.time() 
-        returned_value = func(*args, **kwargs) 
-        end = time.time() 
+
+def calculate_time(func):
+    def inner1(*args, **kwargs):
+        begin = time.time()
+        returned_value = func(*args, **kwargs)
+        end = time.time()
         print("Total time taken in : ", func.__name__, end - begin)
         return returned_value
-    return inner1 
+
+    return inner1
 
 
 def concatenating(m, n):
@@ -15,7 +17,7 @@ def concatenating(m, n):
 
 
 def is_pandigital(n):
-    digital_set = {'1', '2', '3', '4', '5','6', '7', '8', '9'}
+    digital_set = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
 
     n_set = set()
 
@@ -35,15 +37,16 @@ def pandigital_multiples():
     max_pan = 0
     for k in range(2, 10000, 1):
         temp = k
-        
-        for n in range (2, 7):
-            temp = concatenating(temp, k*n)
+
+        for n in range(2, 7):
+            temp = concatenating(temp, k * n)
             if len(str(temp)) > 9:
                 continue
             if is_pandigital(temp) and max_pan < temp:
                 max_pan = temp
                 print(temp)
-    
+
     return max_pan
 
-print(pandigital_multiples())  
+
+print(pandigital_multiples())
